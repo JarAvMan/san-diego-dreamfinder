67575a00ad73793441cbf33e4657183c1df382b8
JarAvMan <jaravman@gmail.com>
2025-04-09 06:01:06 +0000

    Fix Sanity Client

--- san-diego-neighborhood-match/sanityClient.ts
+++ san-diego-neighborhood-match/sanityClient.ts
@@ -1,10 +1,10 @@
 import sanityClient from '@sanity/client';
 
 const client = sanityClient({
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'czumqwya',
   dataset: 'production', // Or your chosen dataset
   useCdn: false,         // `true` if you want cached data; `false` ensures the latest
-  apiVersion: '2023-08-29' // or today's date
+  apiVersion: '2025-04-08' // or today's date
 });
 
 export default client;
